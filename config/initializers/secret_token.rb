9964cb671ed027c07f9f589537660a7b58979ff8
Pravin  Mishra <diatm.pravin.it.07.27@gmail.com>
2012-08-22 23:02:47 +0530

    initail commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActiveresouceApi::Application.config.secret_token = '4ceac6a3c1f8809cb6895cfa6ecaa1cae811ce8ed79a32826b9e88104e1ed130791d0709b075909929f652d4674fc042981af72c1e738902c6715141c67f359e'
